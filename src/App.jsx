8833ecc52ab72eeed6719e2966350cfd550945d1
naleilan <leilan.n@gmail.com>
2024-02-23 10:14:19 +0300

    header done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,36 @@
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="app">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <p>
+          Code Craft <span>101</span>
+        </p>
+        <div className="navbar">
+          <h4>Home</h4>
+          <h4>About</h4>
+          <h4>Projects</h4>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+function Main() {
+  return <main>Main</main>;
+}
+
+function Footer() {
+  return <footer>Footer</footer>;
 }
 
 export default App;
